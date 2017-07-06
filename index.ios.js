311c3a467b3adedebccbc137b4929003cd517289
Jackie Watanabe <sushiigirl@gmail.com>
2017-07-06 15:46:13 -0700

    added firebase setup

--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,23 @@
 // Import a library to help create a component
 
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
-import Header from './src/components/Header';
-import UserPage from './src/components/UserPage'
+// import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+// import Header from './src/components/Header';
+// import UserPage from './src/components/UserPage'
 // import AlbumList from './src/components/AlbumList';
 
 // Create a Component
-const App = () => (
-  <View style={{ flex: 1 }}>
-    <Header />
-    <UserPage />
-  </View>
-);
+// const App = () => (
+//   <View style={{ flex: 1 }}>
+//     <Header />
+//     <UserPage />
+//   </View>
+// );
 
 //Render it to the device
+
+// AppRegistry.registerComponent('photo52', () => App);
 
 AppRegistry.registerComponent('photo52', () => App);
---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
+class App extends Component {
+  componentWillMount() {
+      firebase.initializeApp({
+      apiKey: 'AIzaSyCzVJ58tgRgTsbjnYNeX1WR16pWUqybvGw',
+      authDomain: 'photo52authentication.firebaseapp.com',
+      databaseURL: 'https://photo52authentication.firebaseio.com',
+      projectId: 'photo52authentication',
+      storageBucket: 'photo52authentication.appspot.com',
+      messagingSenderId: '351350443180'
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>Photo52 App</Text>
+      </View>
+
+    );
+  }
+}
+
+export default App;

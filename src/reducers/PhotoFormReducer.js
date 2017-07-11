bbcf6df6bca19467df746b515e93ad37b4d5a193
Jackie Watanabe <sushiigirl@gmail.com>
2017-07-10 21:27:29 -0700

    updated PhotoFormReducer

--- src/reducers/PhotoFormReducer.js
+++ src/reducers/PhotoFormReducer.js
@@ -2,10 +2,16 @@ import {
   PHOTO_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case PHOTO_UPDATE:
+      return { ...state, [action.payload.prop]: action.payload.value };
     default:
       return state;
   }

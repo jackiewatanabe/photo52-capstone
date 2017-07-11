3fd8fa91734d8027fac773e7f06a1be26708101c
Jackie Watanabe <sushiigirl@gmail.com>
2017-07-10 23:11:46 -0700

    dynamic datasource building

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { photosFetch } from '../actions';
 
 class PhotoList extends Component {
   componentWillMount() {
     this.props.photosFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props
+    // that this componenet will be rendered with
+    //  this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ photos }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.clonewithRows(photos);
   }
 
   render() {

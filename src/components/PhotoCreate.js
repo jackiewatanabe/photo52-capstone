93647293018330974b0d29094e32b57d0d7150aa
Jackie Watanabe <sushiigirl@gmail.com>
2017-07-10 21:37:32 -0700

    tied photoUpdate action to PhotoCreate component

--- src/components/PhotoCreate.js
+++ src/components/PhotoCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
+import { photoUpdate } from '../actions';
 
 
 class PhotoCreate extends Component {
@@ -11,6 +13,8 @@ class PhotoCreate extends Component {
           <Input
             label="Name"
             placeholder="Jane"
+            value={this.props.name}
+            onChangeText={text => this.props.photoUpdate({ prop: 'name', value: text })}
           />
         </CardSection>
 
@@ -18,6 +22,8 @@ class PhotoCreate extends Component {
         <Input
           label="Phone"
           placeholder="555-555-5555"
+          value={this.props.phone}
+          onChangeText={text => this.props.photoUpdate({ prop: 'phone', value: text })}
         />
         </CardSection>
 
@@ -33,4 +39,10 @@ class PhotoCreate extends Component {
 
 }
 
-export default PhotoCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.photoForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { photoUpdate})(PhotoCreate);

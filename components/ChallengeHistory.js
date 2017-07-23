2e4b15e80eced07e142b5baa7a61aad60d7996a9
Jackie Watanabe <sushiigirl@gmail.com>
2017-07-22 22:20:57 -0700

    added stars to progress tracking page

--- components/ChallengeHistory.js
+++ components/ChallengeHistory.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { View, Text, LayoutAnimation, TouchableWithoutFeedback } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
@@ -9,11 +10,38 @@ import * as actions from '../actions';
 class ChallengeComplete extends Component {
 
 render(){
+  // const myStarIcon = (<Icon name="stars" size={60} color="#000" style={{ alignSelf: 'center', textAlign: 'center' }} />);
+
+  const stars = [];
+
+  let starCount = 0;
+
+  if (this.props.challenge !== null) {
+    starCount = (this.props.week - 1);
+  } else {
+    starCount = (this.props.week);
+  }
+
+
+  _.times(starCount, () => {
+  stars.push(<Icon name="stars" size={55} color="#000" style={{ alignSelf: 'center', textAlign: 'center' }} />);
+  });
+
+  _.times((52 - starCount), () => {
+  stars.push(<Icon name="stars" size={55} color="#D8D8D8" style={{ alignSelf: 'center', textAlign: 'center' }} />);
+  });
+
   return (
     <Card>
-      <CardSection>
+      <CardSection style={{ alignSelf: 'center' }}>
         <View>
-          <Text>COMPLETED CHALLENGES</Text>
+          <Text style={{ alignSelf: 'center', fontFamily: 'Iowan Old Style', fontSize: 25 }}>COMPLETED CHALLENGES</Text>
+        </View>
+      </CardSection>
+      <CardSection style={{ alignSelf: 'center' }}>
+        <View style={{ alignSelf:
+        'center' }}>
+          <Text>{stars}</Text>
         </View>
       </CardSection>
     </Card>
@@ -22,10 +50,10 @@ render(){
 }
 
 const mapStateToProps = ({ auth }) => {
-  const { week } = auth;
+  const { week, challenge } = auth;
   const expanded = false;
 
-  return { week };
+  return { week, challenge };
 };
 
 export default connect(mapStateToProps, actions)(ChallengeComplete);
